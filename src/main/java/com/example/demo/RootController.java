a4fabb23ff79a9ec9b03fa49ed0b33410ebfa588
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-20 18:04:37 +0900

    project init

---
+++ src/main/java/com/example/demo/RootController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+
+}
